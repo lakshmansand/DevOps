960f22f7faaba4a89f06cec2770489ecad7249d6
lakshmansandeep006 <lakshmansandeep006@gmail.com>
2025-03-14 21:52:46 +0530

    frontend 3

--- Expense_project_bash_script/forntend.sh
+++ Expense_project_bash_script/forntend.sh
@@ -5,6 +5,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-cp /home/ec2-user/batch58/DevOps/Expense_project_bash_script/proxy.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/DevOps/Expense_project_bash_script/proxy.conf /etc/nginx/default.d/expense.conf
 systemctl restart nginx 
 echo " ** frontend installation completed** "cl
