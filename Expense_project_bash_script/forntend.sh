522a7b277f2df41e47e63fbfc7e3f5df5a4bf483
lakshmansandeep006 <lakshmansandeep006@gmail.com>
2025-03-14 21:30:36 +0530

    Automating frontend component

---
+++ Expense_project_bash_script/forntend.sh
@@ -0,0 +1,10 @@
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+cp /home/ec2-user/batch58\DevOps\Expense_project_bash_script/proxy.conf /etc/nginx/default.d/expense.conf
+systemctl restart nginx 
+echo " ** frontend installation completed** "cl
